d9fc556ea3c0d90f2a8aff6c724813c640a302a0
leeeunjin <enzn@hufs.ac.kr>
2025-08-15 14:05:34 +0900

    Initial Commit

---
+++ src/test/java/com/ieunjin/farmlogs/FarmlogsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ieunjin.farmlogs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmlogsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
