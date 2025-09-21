be2b924721f188141974f8234e0a84d636ac83be
leeeunjin <enzn@hufs.ac.kr>
2025-09-21 15:24:40 +0900

    FIX: 한글 아이디 입력 막음

--- src/main/java/com/ieunjin/farmlogs/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ieunjin/farmlogs/exception/GlobalExceptionHandler.java
@@ -1,10 +1,13 @@
 package com.ieunjin.farmlogs.exception;
 
-import com.ieunjin.farmlogs.dto.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 
 @Slf4j
 @RestControllerAdvice
@@ -18,5 +21,24 @@ public class GlobalExceptionHandler {
 
         return ResponseEntity.status(code.getStatus())
                 .body(ErrorResponse.of(code));
+    }
+
+    // dto검증 에러
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationException(MethodArgumentNotValidException ex) {
+        String errorMessage = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .findFirst()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .orElse("잘못된 입력값입니다.");
+
+        ErrorResponse response = new ErrorResponse(
+                HttpStatus.BAD_REQUEST.value(),
+                ErrorCode.INVALID_INPUT_VALUE.name(),
+                errorMessage
+        );
+
+        return ResponseEntity.badRequest().body(response);
     }
 }
